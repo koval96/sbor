74d13a978dc5184d809c6b31230c391624461648
6ocra3 <58596070+6ocra3@users.noreply.github.com>
2022-04-23 13:04:52 +0300

    profile card

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -5,44 +5,36 @@ function Profile() {
         <div className="">
             <div className="profile_header">
                 <div className="profile_name">
-                    <img src={man} width="120px" />
-                    <div className="surname">
+                    <img className="profile_image" src={man} width="120px" />
+                    <div className="surname info_block">
                         <p className="name">Узкий Владимир</p>
-                        <p className="label">Волонтер</p>
+                        <p className="label">Стаж: 2 года</p>
                     </div>
                 </div>
                 <div className="contacts">
-                    <div className="Phone">
+                    <div className="Phone info_block">
                         <p className="label">Телефон</p>
                         <p>+7 (777) 777-77-77</p>
                     </div>
-                    <div className="Telegram">
+                    <div className="Telegram info_block">
                         <p className="label">Telegram</p>
                         <p>@shirokiy</p>
                     </div>
-                    <div className="Sign_date">
-                        <p className="label">Зарегистрирован</p>
-                        <p>19.08.21</p>
-                    </div>
                 </div>
-                <div className="skills">
-                    <div className="hours">
-                        <p className="label">Всего часов</p>
-                        <p>100.5</p>
+                <div className="skills info_block">
+                    <div className="operations">
+                        <p className="label">Количество операций</p>
+                        <p>10</p>
                     </div>
-                    <div className="Courses">
+                    <div className="Courses ">
                         <p className="label">Пройдено курсов</p>
                         <p>12</p>
                     </div>
-                    <div className="Operations">
-                        <p className="label">Зарегистрирован</p>
-                        <p>19.08.21</p>
-                    </div>
                 </div>
-                <div className="last_operation">
-                    <div className="last_participation">
-                        <p className="label">Последняя операция</p>
-                        <p>19.08.2021 в 18:00</p>
+                <div className="last_search">
+                    <div>
+                        <p className="lable">Последяя операция</p>
+                        <p>18.04.2031</p>
                     </div>
                 </div>
             </div>
