69db3713556c4ead4e3bfc8674edd3d54fa1776e
6ocra3 <58596070+6ocra3@users.noreply.github.com>
2022-04-24 10:40:03 +0300

    mod script

--- script.sh
+++ script.sh
@@ -2,8 +2,10 @@
 git clone https://github.com/koval96/sbor
 git checkout install
 cd sbor
-venv/Scripts/activate.bat
+source venv/bin/activate
+source venv/bin/activate
 pip install -r api/requirements.txt
+source venv/bin/activate
 venv/Scripts/activate.bat
 python api/manage.py runserver &
 cd client
