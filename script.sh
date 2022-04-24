eacbc6d6d515aa6fb16294f7c109bc0aa2d3136f
Alexandr Kovalenko <40070892+koval96@users.noreply.github.com>
2022-04-24 11:07:03 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,11 +2,17 @@
 git clone https://github.com/koval96/sbor
 cd sbor
 git checkout install
+cd ..
+python3 -m venv venv
 source venv/bin/activate
 source venv/bin/activate
-pip install -r api/requirements.txt
+cd sbor
+pip3 install -r api/requirements.txt
+pip3 install django-tgbot
+cd sbor
 source venv/bin/activate
-python api/manage.py runserver &
+python3 api/manage.py runserver &
+sleep 6
 cd client
 npm install
 npm start
